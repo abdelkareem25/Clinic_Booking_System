b9d9fb36fd551e0aa289444fc61d5e6eeaa27ad3
Ahmed Elserwy <elserwyahmed081@gmail.com>
2026-09-19 20:49:10 +0300

    Point frontend at correct Render service URL

--- Clinic.Web/src/environments/environment.prod.ts
+++ Clinic.Web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://clinic-api-x2w2.onrender.com/api',
+  apiUrl: 'https://clinic-booking-r70e.onrender.com/api',
   refreshTokenEndpoint: 'Accounts/RefreshToken'
 };
